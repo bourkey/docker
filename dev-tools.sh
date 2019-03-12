6eb224610b4891da06c6f9ac39a2e0ef51f7aa27
Nick Bourke <nbourke@tenable.com>
2019-03-12 13:40:20 +1100

    add to docker

--- dev-tools.sh
+++ dev-tools.sh
@@ -3,7 +3,7 @@
 
 yum -y update
 yum -y install epel-release
-yum -y install which openssl git vim mlocate curl sudo unzip file python-devel python-pip python36 python36-devel wget bind-utils
+yum -y install which openssl git vim mlocate curl sudo unzip file python-devel python-pip python36 python36-devel wget bind-utils kubectl
 useradd -m -u 501 nbourke
 chown nbourke:nbourke /home/nbourke/
 echo '%wheel    ALL=(ALL)    NOPASSWD:ALL' > /etc/sudoers.d/wheel
@@ -15,18 +15,15 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/var/tmp/awscli-bu
 cd /var/tmp/
 unzip awscli-bundle.zip
 cd /var/tmp/awscli-bundle/
- /var/tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
- rm -f /var/tmp/awscli-bundle.zip
- rm -rf /var/tmp/awscli-bundle
+/var/tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+rm -f /var/tmp/awscli-bundle.zip
+rm -rf /var/tmp/awscli-bundle
 
 # Install pip
 cd /var/tmp/
 curl -O https://bootstrap.pypa.io/get-pip.py
 /usr/bin/python3.6 get-pip.py
 
-# install kubectl
-yum -y update
-yum -y install kubectl
 
 # install helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get-helm.sh
